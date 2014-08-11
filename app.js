e107d22c312e83451b34d56179239892c6c19a74
李波 <Tony@libo-imac.lan>
2014-08-11 21:51:11 +0800

    路由放到单独文件中

--- app.js
+++ app.js
@@ -37,15 +37,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/u/:user', routes.user);
-app.post('/post', routes.post);
-app.get('/reg', routes.reg);
-app.post('/reg', routes.doReg);
-app.get('/login', routes.login);
-app.post('/login', routes.doLogin);
-app.get('/logout', routes.logout);
-
+routes(app);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,25 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express'});
-};
-
-exports.user = function(req, res){
-};
-
-exports.post = function(req, res){
-}; 
-
-exports.reg = function(req, res){
-  res.render('reg',{title:'用户注册'});
-};
-
-exports.doReg = function(req, res){
-};
-
-exports.login =  function(req, res){
-};
-
-exports.doLogin = function(req, res){
-};
-
-exports.logout = function(req, res){
+module.exports = function(app){
+    app.get('/',function(req, res){
+      res.render('index',{
+        title:'首页'
+	});
+    });
+    app.get('/reg',function(req, res){
+      res.render('reg',{
+	title:'用户注册'
+	});
+      });
+    app.get('/u/:user',function(req, res){
+	    });
+    app.post('/post',function(req, res){
+	    });
+    app.post('/reg',function(req, res){
+	    });
+    app.get('/login',function(req, res){
+	    });
+    app.post('/login',function(req, res){
+	    });
 };
