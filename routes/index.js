88cf4d5968595750d33b84d66f051ea7cc57ed16
libotony <louding@Tony.local>
2014-08-12 17:11:44 +0800

    session可用

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,17 @@
  */
 
 module.exports = function(app){
-    app.get('/',function(req, res){
+   app.all("*",function(req, res, next){
+		   	var err = req.session.error;
+		   	delete req.session.error;
+			res.locals.message = '';
+			if(err)
+				res.locals.message = '<div class="alert alert-error">'+err+'</div>';
+				console.log('message'+res.locals.message);
+				next();
+
+		   });
+   app.get('/',function(req, res){
       res.render('index',{
         	title:'首页'
 		});
@@ -15,10 +25,16 @@ module.exports = function(app){
 		});
 	});
     app.post('/reg',function(req, res){
+		if((req.body['username'] == '')||(req.body['password-repeat'] == '')||(req.body['password'] == '')){
+			req.session.error = '用户名或密码不得为空！';
+			return res.redirect('/reg');
+		}
 		if(req.body['password-repeat'] != req.body['password']){
 			req.session.error = '两次输入密码不一致！';
+			return res.redirect('/reg');
 			console.log(req.session.error);
 		}
+		console.log(req.body['username']);
 	});
 	app.get('/u/:user',function(req, res){
 	});
